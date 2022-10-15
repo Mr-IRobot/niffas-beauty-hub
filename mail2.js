40806cfbdd32cca187d09727e855c3f864cc31b5
Mr-IRobot <breezeconcept1@gmail.com>
2022-10-15 17:03:29 +0100

    fourth commit

--- mail2.js
+++ mail2.js
@@ -5,11 +5,12 @@ function sendMail(){
         subject: document.getElementById("Subject").value,
         phone: document.getElementById("Phone").value,
         email: document.getElementById("Email").value,
+        service: document.getElementById("Service").value,
         message: document.getElementById("Message").value,
     };
-    const serviceID = "service_g5ytdo9";
+    const serviceID = "service_qw19562";
 
-    const templateID = "template_15j68mi";
+    const templateID = "template_o94j1ws";
 
 
 
@@ -21,20 +22,10 @@ function sendMail(){
             document.getElmenetById("Subject").value = "";
             document.getElmenetById("Phone").value = "";
             document.getElmenetById("Email").value = "";
+            document.getElmenetById("Service").value = "";
             document.getElmenetById("Message").value = "";
             console.log(res);
             alert("Message sent successfully");
         }
     ).catch((err) => console.log(err));
 }
-
-
-
-
-
-
-
-
-
-
-
